042623a3f29a564972561ea33f62faf310534f1a
Misan Meggison <misanmeggison@Misans-MacBook-Air.local>
2023-09-08 19:43:50 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import datetime
+import pytz
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def get_info():
+    # Get query parameters
+    slack_name = request.args.get('slack_name')
+    track = request.args.get('track')
+
+    # Get current day of the week
+    current_day = datetime.datetime.now(pytz.utc).astimezone(pytz.timezone('US/Eastern')).strftime("%A")
+
+    # Get current UTC time with validation of +/-2 minutes
+    current_time = datetime.datetime.now(pytz.utc)
+    current_time_str = current_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    # Construct GitHub URLs
+    github_repo_url = "https://github.com/Meggison/stage-one-api/"
+    github_file_url = github_repo_url + "https://github.com/Meggison/stage-one-api/blob/main/endpoint.ext"
+
+    # Prepare JSON response
+    response = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": current_time_str,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
